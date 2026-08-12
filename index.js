7aeb3b4788a030d8672d2ea6737f9a4c84fc2c9f
kiergit <60729046+kiergit@users.noreply.github.com>
2026-08-13 06:05:31 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+function playRandomizer() {
+
+    // Dice 1
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+    if (randomNumber1 === 1) {
+        document.querySelector(".img1").setAttribute('src', './images/dice1.png');
+    } 
+    else if (randomNumber1 === 2) {
+        document.querySelector(".img1").setAttribute('src', './images/dice2.png');
+    } 
+    else if (randomNumber1 === 3) {
+        document.querySelector(".img1").setAttribute('src', './images/dice3.png');
+    }
+    else if (randomNumber1 === 4) {
+        document.querySelector(".img1").setAttribute('src', './images/dice4.png');
+    }
+    else if (randomNumber1 === 5) {
+        document.querySelector(".img1").setAttribute('src', './images/dice5.png');
+    }
+    else if (randomNumber1 === 6) {
+        document.querySelector(".img1").setAttribute('src', './images/dice6.png');
+    }
+
+    // Dice 2
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    if (randomNumber2 === 1) {
+        document.querySelector(".img2").setAttribute('src', './images/dice1.png');
+    } 
+    else if (randomNumber2 === 2) {
+        document.querySelector(".img2").setAttribute('src', './images/dice2.png');
+    } 
+    else if (randomNumber2 === 3) {
+        document.querySelector(".img2").setAttribute('src', './images/dice3.png');
+    }
+    else if (randomNumber2 === 4) {
+        document.querySelector(".img2").setAttribute('src', './images/dice4.png');
+    }
+    else if (randomNumber2 === 5) {
+        document.querySelector(".img2").setAttribute('src', './images/dice5.png');
+    }
+    else if (randomNumber2 === 6) {
+        document.querySelector(".img2").setAttribute('src', './images/dice6.png');
+    }
+
+    // Winner
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+        document.querySelector(".flag2").style.display = "block";
+        document.querySelector(".flag1").style.display = "none";
+    } else if (randomNumber2 > randomNumber1) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+        document.querySelector(".flag1").style.display = "block";
+        document.querySelector(".flag2").style.display = "none";
+    } else if (randomNumber1 === randomNumber2){
+        document.querySelector("h1").innerHTML = "Draw!";
+        document.querySelector(".flag1").style.display = "none";
+        document.querySelector(".flag2").style.display = "none";
+    }
+
+
+}
+
+
